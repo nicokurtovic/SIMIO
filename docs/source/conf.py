164214eae3e4fa312d13657fcf1b1be23e1e4e1a
Nicolás Kurtovic <nicokurtovic@gmail.com>
2022-07-31 02:12:58 +0200

    Updated conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'SIMIO-continuum'
+copyright = '2022, Nicolas Kurtovic'
+author = 'Nicolas'
 
-release = '0.1'
-version = '0.1.0'
+release = '1.0'
+version = '1.1.0'
 
 # -- General configuration
 
