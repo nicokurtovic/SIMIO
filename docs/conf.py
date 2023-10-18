9e969f4457763158545300add44e2b65ee4602f0
Nicolás Kurtovic <nicokurtovic@gmail.com>
2023-10-18 20:22:15 -0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,7 @@ master_doc = 'index'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-#exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # Make sure the functions are in the order in the .py file.
 add_function_parentheses = True
