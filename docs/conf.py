a11bdf39bf743e3fa6761b0193d823a92c2a08cb
Nicolás Kurtovic <nicokurtovic@gmail.com>
2023-10-18 19:05:47 -0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,8 @@ extensions = [
 ]
 
 # Mathjax path
-mathjax_path = 'https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'
+#mathjax_path = 'https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'
+mathjax_path = 'http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'
 
 # astropy
 autodoc_mock_imports = ['astropy']
